61bd7bde4390c8d9937853d20c588d938ef7453b
bryant <bryant0918@gmail.com>
2023-10-18 12:00:38 -0500

    modified deploy test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,15 @@
 rails db:migrate
-git clone https://github.com/llnl/metall.git
+
+# Download Boost (Boost C++ Libraries 1.64 or more is required)
+# One can skip this step if Boost is already available.
+wget https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz
+tar xvf boost_1_78_0.tar.gz
+export BOOST_ROOT=$PWD/boost_1_78_0
+
+git clone https://github.com/LLNL/metall
+export METALL_INCLUDE=$PWD/metall/include
+
+g++ -std=c++17 your_program.cpp -lstdc++fs -I${BOOST_ROOT} -I${METALL_INCLUDE}
+
 pwd
 touch howdy
